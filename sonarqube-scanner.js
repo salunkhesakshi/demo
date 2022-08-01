f59f70619a7737a00e293014fb2e6330c1c3b458
saksalun <sakshi.salunkhe@publicissapient.com>
2022-08-01 12:50:21 +0530

    changes

--- sonarqube-scanner.js
+++ sonarqube-scanner.js
@@ -1,9 +1,9 @@
 const scanner = require('sonarqube-scanner');
 scanner(
   {
-  serverUrl: "http://localhost:9000",
+  serverUrl: "http://107.20.130.141:9000/",
   login:"admin",
-  password:"admin",
+  password:"sonar",
   options: {
     "sonar.sources": "./src"
   },
